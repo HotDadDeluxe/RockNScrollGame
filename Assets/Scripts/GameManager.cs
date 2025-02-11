5abc35adee960de9ac2afad1a3d51e8a9b1ed294
HotDadDeluxe <adevens@unm.edu>
2025-02-11 15:36:07 -0700

    main menu added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void Play() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //GameScene
+        Debug.Log("Play button clicked");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); //OptionsScene
+        Debug.Log("Options button clicked");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit button clicked");
+    }
+}

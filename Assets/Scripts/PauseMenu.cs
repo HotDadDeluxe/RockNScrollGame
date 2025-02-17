18afe979ba480c4a09217e4bf16d533fce325e4d
kmymsho <shokomiyama0227@gmail.com>
2025-02-16 22:13:56 -0700

    added eventsystem to fix issue with pause menu button

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -7,21 +7,7 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
     public static bool isPaused = false;
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     pauseMenu.SetActive(false);
-    //     if (pauseMenu == null)
-    //     {
-    //         Debug.LogError("Pause Menu UI is not assigned in the inspector!");
-    //     }
-    //     else
-    //     {
-    //         Debug.LogWarning("Pause Menu UI is not assigned in the inspector!");
-    //     }
-    // }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -37,27 +23,27 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    void pauseGame()
+    public void pauseGame()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
     }
 
-    void resumeGame()
+    public void resumeGame()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
     }
 
-    void gotoMainMenu()
+    public void gotoMainMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenuScene");
     }
 
-    void quitGame()
+    public void quitGame()
     {
         Application.Quit();
     }
